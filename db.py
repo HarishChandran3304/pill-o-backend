d7d498a7eacf4625e42edd1976fc0ad587e96503
harish.rajkumar2022@vitstudent.ac.in <harish.rajkumar2022@vitstudent.ac.in>
2024-02-20 01:35:37 +0530

    Update prescription schema

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import random
 import os
+from datetime import datetime
 
 
 load_dotenv()
@@ -14,18 +15,23 @@ users = db["users"]
 
 
 def get_prescription(id: int):
-    return prescriptions.find_one({"prescriptionID": id}, {"_id": 0})
+    prescription = prescriptions.find_one({"prescriptionID": id}, {"_id": 0})
+    for medicine in prescription["medicines"]:
+        medID = medicine["medID"]
+        medicine.update(get_medicine(medID))
+    return prescription
 
 def get_medicine(id: int):
     return medicines.find_one({"medID": id}, {"_id": 0})
 
 def update_encoding(prescription_id: int):
     encoding = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=25))
-    prescriptions.update_one({"prescriptionID": prescription_id}, {"$set": {"encoding": encoding}})
+    prescriptions.update_one({"prescriptionID": prescription_id}, {"$set": {"encoding": encoding, "time": datetime.now()}})
     return {"encoding": encoding}
 
 def verify_encoding(prescription_id: int, encoding: str):
     return prescriptions.find_one({"prescriptionID": prescription_id}, {"_id": 0})["encoding"] == encoding
 
 def get_user_prescriptions(id: int):
-    return [prescriptions.find_one({"prescriptionID": prescription}, {"_id": 0}) for prescription in users.find_one({"userID": id}, {"_id": 0})["prescriptions"]]
+    x = [get_prescription(prescription_id) for prescription_id in users.find_one({"userID": id}, {"_id": 0})["prescriptions"]]
+    return x
